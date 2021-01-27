51c8a1579d2a03cc3e0265598032232a4188965c
agarwarahul-22 <ag.rahul223@gmail.com>
2021-01-27 21:31:46 +0530

    del

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 ReactDOM.render(
