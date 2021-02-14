4c4082d74709091f7210ca1536f37a4774af5123
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-02-14 21:18:27 +0530

    Update LeaderComponent.js

--- src/components/LeaderComponent.js
+++ src/components/LeaderComponent.js
@@ -97,7 +97,7 @@ class Leader extends Component {
     console.log("hi");
     axios
       .get(
-        "https://thingproxy.freeboard.io/fetch/https://gc2021iitbbs.herokuapp.com/leaderboard"
+        "http://gobetween.oklabs.org/https://gc2021iitbbs.herokuapp.com/leaderboard"
       )
       .then((response) => {
         console.log("hi");
